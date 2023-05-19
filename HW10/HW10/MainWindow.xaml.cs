28049661fadaf1286dae0f2ffb3954071a40e2c7
VladyslavGontarenko <122695793+VladyslavGontarenko@users.noreply.github.com>
2023-05-19 17:09:11 +0200

    HW10

---
+++ HW10/HW10/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using MaterialDesignThemes.Wpf;
+
+namespace HW10
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Применение темы Material Design
+            var paletteHelper = new PaletteHelper();
+            ITheme theme = paletteHelper.GetTheme();
+            theme.SetBaseTheme(Theme.Dark);
+            paletteHelper.SetTheme(theme);
+        }
+    }
+}
